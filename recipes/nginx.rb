e986d0b3ff43500b180a7508b1e44e323155a733
Brent Geach <bgeach@ur-sltn.com>
2017-04-21 14:23:07 +0100

    fixed nginx

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -11,7 +11,7 @@ remote_directory '/etc/nginx2' do
   owner 'root'
   group 'root'
   recursive true
-  notifies :reload, service[nginx], :immediately
+  notifies :restart, 'service[nginx]', :immediately
 end
  
 directory '/var/lib/nginx/tmp/client_body' do
@@ -19,6 +19,5 @@ directory '/var/lib/nginx/tmp/client_body' do
 end
  
 service 'nginx' do
-  action [:enabled, :start]
-  
+  action [:enable, :start]
 end
