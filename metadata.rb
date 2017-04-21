4302457b4d231b6bce3437842155ff4de7437d73
Brent Geach <bgeach@ur-sltn.com>
2017-04-21 14:35:24 +0100

    fixed php

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bgeach@ur-sltn.com'
 license 'All Rights Reserved'
 description 'Installs/Configures webnodes'
 long_description 'Installs/Configures webnodes'
-version '0.1.3'
+version '0.1.4'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- recipes/php7.rb
+++ recipes/php7.rb
@@ -4,9 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 remote_directory '/etc/php2' do
-  source 'nginx'
+  source 'php'
   action :create
   owner 'root'
   group 'root'
   recursive true
+  notifies :restart, 'service[php7.0-fpm]', :immediately
+end
+
+service 'php7.0-fpm' do
+  action [:enable, :start]
 end
